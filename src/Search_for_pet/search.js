362eb11a0ed5653f1a4acf1992c2b5aa707efd00
Robert <robertortizarias@gmail.com>
2025-10-21 22:12:50 -0400

    fix: buscar mascotas test corregido

---
+++ src/Search_for_pet/search.js
@@ -0,0 +1,6 @@
+function buscar(name, age, breed)
+{
+    return [name, age, breed];
+}
+
+export default buscar;
--- src/Search_for_pet/search.spec.js
+++ src/Search_for_pet/search.spec.js
@@ -1,31 +1,23 @@
 // tests/registerPet.test.js
-import { registrar, registerPet } from '../src/Register_Pet/registerPet.js';
+import buscar from '../Search_for_pet/search.js';
+describe('Busqueda de mascota', () => {
 
-describe('Registro de mascota', () => {
-
-    it('debe registrar el nombre de una mascota', () => {
-    // Esperamos que registrar retorne el mensaje correcto
-    expect(() => registrar('Luna')).toThrow('¡Mascota Registrada!');
+    it('debe buscar el nombre de una mascota', () => {
+    // Esperamos que buscar retorne el mensaje correcto
+    expect(() => buscar('Max')).toThrow('¡Mascota Encontrada!');
     });
 
-  it('debe fallar si la edad no es válida', async () => {
-    const petData = { nombre: 'Luna', edad: -2, raza: 'Labrador', descripcion: 'Muy juguetona' };
-    await expect(registerPet(petData, 1))
-      .rejects
-      .toThrow('Edad inválida');
-  });
+//   it('debe fallar si la edad no es válida', async () => {
+//     const petData = { nombre: 'Luna', edad: -2, raza: 'Labrador' };
+//     await expect(registerPet(petData, 1))
+//       .rejects
+//       .toThrow('Edad inválida');
+//   });
 
-  it('debe registrar correctamente una mascota', async () => {
-    const petData = { nombre: 'Rocky', edad: 4, raza: 'Bulldog', descripcion: 'Tranquilo y amigable' };
-    const result = await registerPet(petData, 1);
+//   it('debe registrar correctamente una mascota', async () => {
+//     const petData = { nombre: 'Rocky', edad: 4, raza: 'Bulldog' };
+//     const result = await registerPet(petData, 1);
 
-    expect(result).toHaveProperty('id');
-    expect(result).toMatchObject({
-      nombre: 'Rocky',
-      edad: 4,
-      raza: 'Bulldog',
-      descripcion: 'Tranquilo y amigable',
-      userId: 1
-    });
-  });
+//     });
+//   });
 });
