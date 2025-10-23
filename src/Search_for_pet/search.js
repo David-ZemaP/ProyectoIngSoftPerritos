477fbcee0c3946aae5dfe4bd782ed0ecba89c201
NicoleUg <sofia.ugarte@ucb.edu.bo>
2025-10-22 22:43:14 -0400

    feat: Agregar search para probar front

--- src/Search_for_pet/search.js
+++ src/Search_for_pet/search.js
@@ -1,9 +1,32 @@
-function buscar(name)
-{
-    const nombre = 'Max';
-    if (name === nombre) {
-        throw new Error('¡Mascota Encontrada!');
+function buscar({ nombre, especie, genero, edad, raza }) {
+    const mascota = {
+        nombre: 'Max',
+        edad: 3,
+        raza: 'Bulldog',
+        especie: 'Perro',
+        genero: 'Macho'
+    };
+
+
+    if (nombre && nombre === mascota.nombre) {
+        throw new Error('¡Mascota Encontrada por nombre!');
     }
+
+    if (especie && especie === mascota.especie) {
+        throw new Error('¡Mascota Encontrada por especie!');
+    }
+    if (edad !== undefined && edad === mascota.edad) {
+        throw new Error('¡Mascota Encontrada por edad!');
+    }
+
+    if (raza && raza === mascota.raza) {
+        throw new Error('¡Mascota Encontrada por raza!');
+    }
+
+    if (genero && genero === mascota.genero) {
+        throw new Error('¡Mascota Encontrada por género!');
+    }
+
     return '¡Mascota No Encontrada!';
 }
 
