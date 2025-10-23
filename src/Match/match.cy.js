197b5beadd9f4afd9c3f2ae0001391cb400c72c5
David-ZemaP <david@David.>
2025-10-22 22:55:03 -0400

    test: green test para el check match

---
+++ src/Match/match.cy.js
@@ -0,0 +1,6 @@
+describe('ATDD: Flujo de Match de Mascota', () => {
+  beforeEach(() => {
+    // Asume que el servidor web sirve index.html en la raíz
+    cy.visit('match.html');
+  });
+});
---
+++ src/Match/match.spec.js
@@ -0,0 +1,10 @@
+import { checkMatch } from './match.js';
+
+describe('TDD: Lógica Central de Match (checkMatch)', () => {
+  it('Debe retornar TRUE para el ID que genera Match ("pet-3")', () => {
+    // La prueba TDD valida la regla de negocio: pet-3 siempre es un match.
+    const petIdToLike = 'pet-3';
+    const result = checkMatch(petIdToLike);
+    expect(result).toBe(true);
+  });
+});
