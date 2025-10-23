1f79073231d1354dd2c561d9e67f98e073674b76
NicoleUg <sofia.ugarte@ucb.edu.bo>
2025-10-22 23:06:51 -0400

    fix: nombres y duplicacion de archivos

--- src/Search_for_pet/search.js
+++ src/Search_for_pet/search.js
@@ -1,10 +1,8 @@
-function buscar({ nombre, especie, genero, edad, raza }) {
+function buscar({ nombre, edad, raza }) {
     const mascota = {
         nombre: 'Max',
         edad: 3,
-        raza: 'Bulldog',
-        especie: 'Perro',
-        genero: 'Macho'
+        raza: 'Bulldog'
     };
 
 
@@ -12,19 +10,13 @@ function buscar({ nombre, especie, genero, edad, raza }) {
         throw new Error('¡Mascota Encontrada por nombre!');
     }
 
-    if (especie && especie === mascota.especie) {
-        throw new Error('¡Mascota Encontrada por especie!');
-    }
+
     if (edad !== undefined && edad === mascota.edad) {
         throw new Error('¡Mascota Encontrada por edad!');
     }
 
     if (raza && raza === mascota.raza) {
         throw new Error('¡Mascota Encontrada por raza!');
-    }
-
-    if (genero && genero === mascota.genero) {
-        throw new Error('¡Mascota Encontrada por género!');
     }
 
     return '¡Mascota No Encontrada!';
