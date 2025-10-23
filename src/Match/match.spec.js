56fb0eca7d9ba2a9bcdbb7b5afc5b02e19f3044e
David-ZemaP <david@David.>
2025-10-22 23:06:05 -0400

    fix: fix test of checkmatch

--- src/Match/match.spec.js
+++ src/Match/match.spec.js
@@ -2,7 +2,7 @@ import { checkMatch } from './match.js';
 
 describe('TDD: Lógica Central de Match (checkMatch)', () => {
   it('Debe retornar TRUE para el ID que genera Match', () => {
-    expect(result).toBe(true);
+    expect(checkMatch('pet-3')).toBe(true);
   });
 
   it('Debe retornar TRUE para el ID que genera Match ("pet-3")', () => {
@@ -10,5 +10,11 @@ describe('TDD: Lógica Central de Match (checkMatch)', () => {
     const petIdToLike = 'pet-3';
     const result = checkMatch(petIdToLike);
     expect(result).toBe(true);
+  });
+
+  it('Debe retornar FALSE para un ID que no genera Match ("pet-5")', () => {
+    const petIdToLike = 'pet-5';
+    const result = checkMatch(petIdToLike);
+    expect(result).toBe(false);
   });
 });
